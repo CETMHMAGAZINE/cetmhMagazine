0f404dbc1465e27e7f744be5a522d13e90b2addc
CETMHMAGAZINE <67359416+CETMHMAGAZINE@users.noreply.github.com>
2020-06-24 11:39:14 +0530

    Add files via upload

---
+++ sr.js
@@ -0,0 +1,15 @@
+function test_str() {
+            var res;
+            var str =
+                document.getElementById("t1").value;
+            if (str=="hatersmakeusfamous"){
+                window.location="https://drive.google.com/folderview?id=1Zq0NPnO13Q1TipKOqSwuCMulVUVBBMYc";
+
+                res = "LOADING......";}
+            else{
+
+                res = "INCORRECT PASSWORD."; }
+              document.getElementById("t2").value = res;
+
+        }
+
